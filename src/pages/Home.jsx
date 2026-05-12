985ec9c5eb996245767cff49ccc8e28ace909a04
Mansvi <mansvi292005@gmail.com>
2026-05-12 15:19:19 +0530

    web

---
+++ src/pages/Home.jsx
@@ -0,0 +1,19 @@
+import "../styles/pages/home.css";
+
+function Home() {
+  return (
+    <div className="home">
+      <div className="hero-section">
+        <h1>SecurExam</h1>
+        <p>AI Powered Secure Online Examination Platform</p>
+
+        <div className="hero-buttons">
+          <button>Get Started</button>
+          <button>Learn More</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Home;
